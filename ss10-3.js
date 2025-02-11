fadefca142df526746989838abeef0fb54b4db9d
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-02-11 23:22:33 +0900

    upload btvn ss10-3

---
+++ ss10-3.js
@@ -0,0 +1,5 @@
+let ss10= "wellcome to Rikkei Academy";
+console.log(ss10);
+document.write(x="Quyết tâm học lập trình javascript");
+alert(s="Chào mừng bạn đến với học viện Rikkei Academy");
+let c= prompt("bạn đã đủ 18 tuổi chưa! vui lòng xác nhận yes hoặc no");
