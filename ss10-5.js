1757c38740ef359888d5647af93b3d7f0ce71916
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-02-11 23:40:36 +0900

    upload btvn ss10-5

---
+++ ss10-5.js
@@ -0,0 +1,4 @@
+let firstNumber= prompt("hãy nhập 1 số bất kỳ mà bạn muốn");
+let secondNumber= prompt("hãy nhập 1 số bất kỳ mà bạn muốn");
+let x=Number.isInteger (firstNumber % secondNumber);
+alert(x)
