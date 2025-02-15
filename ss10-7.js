a693814d7c978d1019b9c318b22f17637e6dae07
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-02-15 22:42:45 +0900

    upload btvn ss10-7

---
+++ ss10-7.js
@@ -0,0 +1,4 @@
+let math = +prompt("hãy nhập vào điểm toán của bạn");
+let physics = +prompt("hãy nhập vào điểm vật lý của bạn");
+let chemistry = +prompt("hãy nhập vào điểm hoá học của bạn");
+alert("điểm trung bình 3 môn của bạn là: " + (math+physics+chemistry)/3 + "điểm");
